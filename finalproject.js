bbbe496c3bad4fc15d60af3707e9d3aac65e0591
amb233 <amb233@maroon34.cs.calvin.edu>
2020-01-20 09:14:36 -0500

    hey

--- finalproject.js
+++ finalproject.js
@@ -5,3 +5,18 @@ function myFunction(){
 
 d3.select('#button').on('click' , myFunction)
 
+d3.select("body") 
+  .append("svg")
+  .attr("width", 100)
+  .attr("height" , 100)
+  .append('circle')
+  .attr('cx', 50)
+  .attr('cy', 50)
+  .attr('r', 50)
+  .style("fill", 'blue')
+
+
+pieData = [{"label": "one", "value":50}
+           {"label": "two", "value":30}
+           {"label": "three", "value":20}]
+
